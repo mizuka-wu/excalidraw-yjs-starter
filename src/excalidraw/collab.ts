31cbb1a434d3706a886217f02298581603d7408c
Edward Mizuka <mizuka.wu@outlook.com>
2025-05-25 01:30:55 +0800

    feat: 添加基础的支持能力

--- src/excalidraw/collab.ts
+++ src/excalidraw/collab.ts
@@ -1,13 +1,86 @@
 import { useEffect, useState } from "react";
-// import { IndexeddbPersistence } from "y-indexeddb";
-// import { SocketIOProvider } from "y-socket.io";
+import * as Y from "yjs";
+import { ExcalidrawBinding } from "y-excalidraw";
+import { Awareness } from "y-protocols/awareness";
+
+import { IndexeddbPersistence } from "y-indexeddb";
+import { SocketIOProvider } from "y-socket.io";
+import type { RefObject } from "react";
 import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
 
-export const useCollab = () => {
+interface CollabOptions {
+  id?: string;
+  isUseIndexedDb?: boolean;
+}
+
+export const useCollab = (
+  excalidrawRef: RefObject<HTMLElement | null>,
+  options: CollabOptions = {}
+) => {
   const [api, setApi] = useState<ExcalidrawImperativeAPI | null>(null);
+  const [binding, setBinding] = useState<ExcalidrawBinding | null>(null);
+
+  const id = options.id ?? "";
+  const isUseIndexedDb = options.isUseIndexedDb ?? false;
+
   useEffect(() => {
     if (!api) return;
-  }, [api]);
 
-  return setApi;
+    const ydoc = new Y.Doc();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const yElements = ydoc.getArray<Y.Map<any>>("elements"); // structure = {el: NonDeletedExcalidrawElement, pos: string}
+    const yAssets = ydoc.getMap("assets");
+
+    const indexeddbPersistence =
+      isUseIndexedDb || !id
+        ? new IndexeddbPersistence(`excalidraw-${id || "default"}`, ydoc)
+        : null;
+
+    const url = `${location.protocol === "https:" ? "wss" : "ws"}://${
+      location.host
+    }/collab`;
+    const socketIoProvider = id
+      ? new SocketIOProvider(url, id, ydoc, {
+          autoConnect: true,
+        })
+      : null;
+
+    const undoManagerOptions = !!(
+      excalidrawRef.current &&
+      excalidrawRef.current.querySelector(".undo-redo-buttons")
+    )
+      ? {
+          excalidrawDom: excalidrawRef.current,
+          undoManager: new Y.UndoManager(yElements),
+        }
+      : undefined;
+
+    const binding = new ExcalidrawBinding(
+      yElements,
+      yAssets,
+      api,
+      socketIoProvider?.awareness || new Awareness(ydoc),
+      undoManagerOptions
+    );
+
+    const originDestroy = binding.destroy;
+    binding.destroy = () => {
+      originDestroy.call(binding);
+      indexeddbPersistence?.destroy();
+      socketIoProvider?.destroy();
+    };
+
+    setBinding(binding);
+    return () => {
+      setBinding(null);
+      binding.destroy();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [api, excalidrawRef]);
+
+  return {
+    setApi,
+    binding,
+    excalidrawRef,
+  };
 };
--- src/excalidraw/index.tsx
+++ src/excalidraw/index.tsx
@@ -1,13 +1,23 @@
 "use client";
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { Excalidraw } from "@excalidraw/excalidraw";
-
 import "@excalidraw/excalidraw/index.css";
 
-const ExcalidrawWrapper: FC = () => {
+import { useCollab } from "./collab";
+
+const ExcalidrawWrapper: FC<{
+  id?: string;
+  isUseIndexedDb?: boolean;
+}> = ({ id, isUseIndexedDb }) => {
+  const excalidrawRef = useRef(null);
+  const { setApi, binding } = useCollab(excalidrawRef, { id, isUseIndexedDb });
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <Excalidraw />
+    <div ref={excalidrawRef} style={{ height: "100vh", width: "100vw" }}>
+      <Excalidraw
+        excalidrawAPI={setApi}
+        isCollaborating={!!binding}
+        onPointerUpdate={binding?.onPointerUpdate}
+      />
     </div>
   );
 };
